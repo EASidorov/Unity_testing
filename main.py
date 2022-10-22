c56516a642a5ee8995d1331c98240b41f65f756d
egorsidorov <twisteroid@gmail.com>
2022-10-22 21:24:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import unittest
+import time
+
+from altunityrunner import *
+
+
+class SecurityRoom():
+    def __init__(self, altdriver):
+        self.altdriver = altdriver
+
+    def load(self):
+        return self.altdriver.load_scene('SecurityRoom')
+
+    @property
+    def guard(self):
+        return self.altdriver.wait_for_object(By.NAME, 'Guard')
+
+    @property
+    def VO(self):
+        return self.altdriver.find_object(By.NAME, 'VO')
+
+
+class Market():
+    def __init__(self, altdriver):
+        self.altdriver = altdriver
+
+    def load(self):
+        return self.altdriver.load_scene('Market')
+
+
+class MyFirstTest(unittest.TestCase):
+    altdriver = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.altdriver = AltUnityDriver()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.altdriver.stop()
+
+    # def test_move_to_guard(self):
+    #     Secrom = SecurityRoom(self.altdriver)
+    #     Secrom.load()
+    #     self.altdriver.click(Secrom.guard.get_screen_position())
+    #     time.sleep(5)
+    #     if Secrom.VO.get_component_property("UnityEngine.AudioSource", "clip", 'UnityEngine.AudioModule'):
+    #         return True
+
+    # def test_coin_pick_up(self):
+    #     self.altdriver.find_object(By.NAME, 'DoorToMarketInteractable').click()
+    #     time.sleep(5)
+    #     self.altdriver.find_object(By.NAME, 'CoinInteractable').click()
+    #     time.sleep(5)
+    #     inv1 = self.altdriver.find_object(By.NAME, 'ItemSlot0/ItemImage')
+    #     if inv1.get_component_property('UnityEngine.UI.Image', 'sprite', 'UnityEngine.UI'):
+    #         return True
+
+    #
+    # def test_go_to_security_room(self):
+    #     Market(self.altdriver).load()
+    #     self.altdriver.find_object(By.NAME, 'DoorToSecurityRoom').click()
+    #     if self.altdriver.get_current_scene() == 'SecurityRoom':
+    #         return True
+
+
